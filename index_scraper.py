c2791b1f9205b312e880f0e2d30d093fb1278a77
BOB-AI-Devs <hanfromfast@gmail.com>
2026-09-23 18:00:17 +0530

    Update index_scraper.py

--- index_scraper.py
+++ index_scraper.py
@@ -347,9 +347,9 @@ def main():
         st.session_state["last_selection"] = (category, name, view_value, pred_value)
         _run_analysis(category, name, view_value, pred_value)
         dark_rgb = (
-            random.randint(30, 80),
-            random.randint(30, 80),
-            random.randint(30, 80)
+            random.randint(50, 80),
+            random.randint(50, 80),
+            random.randint(50, 80)
         )
         st.session_state.dark_rgb = dark_rgb
 
