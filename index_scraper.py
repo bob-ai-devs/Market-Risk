63625e5d9cd55900783f89bc6d1609abc53241d2
BOB-AI-Devs <hanfromfast@gmail.com>
2026-09-24 12:17:44 +0530

    Update index_scraper.py

--- index_scraper.py
+++ index_scraper.py
@@ -174,8 +174,9 @@ def _run_analysis(category: str, name: str, view_value: str, pred_value: str):
     
     st.session_state["task_name"] = name
 
-    if "etf" not in st.session_state.task_name:
-        df = df.drop('iNAV', axis=1)
+    # if "etf" not in st.session_state.task_name:
+    #     df = df.drop('iNAV', axis=1)
+    df = df.drop('iNAV', axis=1)
     
     st.session_state["full_df"] = df
     st.session_state["final_df"] = df.copy()
