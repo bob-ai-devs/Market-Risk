80c48e93d7ff95c650cedd070b5a3b888d57e564
BOB-AI-Devs <hanfromfast@gmail.com>
2026-09-24 13:35:55 +0530

    Update genai_utils.py

--- genai_utils.py
+++ genai_utils.py
@@ -29,7 +29,8 @@ def list_model_names(client: genai.Client):
     # except Exception:
     #     pass
     # return sorted(set(names))
-    return set(['gemini-flash-lite-latest', 'gemini-flash-latest'])
+    # return set(['gemini-flash-lite-latest', 'gemini-flash-latest'])
+    return set(['gemini-flash-lite-latest'])
 
 
 def generate_text(client: genai.Client, prompt: str, model: str = "") -> str:
