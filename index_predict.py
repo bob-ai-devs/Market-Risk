d79336719c3ee00ae764e559b13697b966205ce9
BOB-AI-Devs <hanfromfast@gmail.com>
2026-09-25 13:53:51 +0530

    Update index_predict.py

--- index_predict.py
+++ index_predict.py
@@ -599,13 +599,13 @@ def main():
     with col2:
         st.code(
             f"""Current : {st.session_state.avg_curr[-1]:.2f}
-    30s     : {st.session_state.avg_30s[-1]:.2f}
-    1m      : {st.session_state.avg_1m[-1]:.2f}
-    2m      : {st.session_state.avg_2m[-1]:.2f}
-    3m      : {st.session_state.avg_3m[-1]:.2f}
-    5m      : {st.session_state.avg_5m[-1]:.2f}
-    10m     : {st.session_state.avg_10m[-1]:.2f}
-    Total   : {st.session_state.avg_total[-1]:.2f}"""
+30s     : {st.session_state.avg_30s[-1]:.2f}
+1m      : {st.session_state.avg_1m[-1]:.2f}
+2m      : {st.session_state.avg_2m[-1]:.2f}
+3m      : {st.session_state.avg_3m[-1]:.2f}
+5m      : {st.session_state.avg_5m[-1]:.2f}
+10m     : {st.session_state.avg_10m[-1]:.2f}
+Total   : {st.session_state.avg_total[-1]:.2f}"""
         )
     st.divider()
     render_table(merged_data)
